4597c213e9ffa79bba002fb8daa82589b78bd261
rkorzen <korzeniewski@gmail.com>
2024-11-05 15:03:42 +0100

    args i kwargs - cwiczenie

--- cwiczenia/cwiczenie_07.py
+++ cwiczenia/cwiczenie_07.py
@@ -1,6 +1,17 @@
+from typing import Callable
+
 numbers = [1, -2, 3, 4, -7, -5, 12]
+
+def filter_list(numbers: list, func: Callable[[int], bool]) -> list:
+    wynik = []
+    for el in numbers:
+        if func(el):
+            wynik.append(el)
+    return wynik
+
+
 
 
 def test_filter_list():
     assert filter_list(numbers, lambda x: x>0) == [1, 3, 4, 12]
-    assert filter_list(numbers, lambda x: x % 2 == 0) == [2, 4, 12]
+    assert filter_list(numbers, lambda x: x % 2 == 0) == [-2, 4, 12]
---
+++ cwiczenia/kalkulator/cwiczenie_08.py
@@ -0,0 +1,22 @@
+"""
+
+>> formatuj(
+    "napis 1",
+    "napis 2",
+)
+"napis 1\nnapis 2
+
+>> formatuj(
+    "cena produktu to $cena",
+    "stawka vat to $vat",
+    cena=123.25
+    vat=20.22
+)
+
+"cena produkty to 123.25\nstawka vat to 20.22",
+
+
+"""
+
+lista = ["A", "B"]
+print("\n".join(lista))
