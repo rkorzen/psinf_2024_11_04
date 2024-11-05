d3e759a553ee9ad033e40e8ec3a90dc60d4b222f
rkorzen <korzeniewski@gmail.com>
2024-11-05 09:33:37 +0100

    cwiczenie utrwalajace

--- cwiczenia/cwiczenie_03.py
+++ cwiczenia/cwiczenie_03.py
@@ -1,14 +1,27 @@
-lista: list[int] = [1, 2, 3, 4, 5, 11, 13, 12, 27, 23]
+lista: list[int] = [1, 2, 3, 4, 5, 11, 13, 12, 27, 23, 17]
 wynik: list[int] = []
 
 # napisz algorytm ktory wybierze z tej listy tylko liczby pierwsze
 # wynik.append(2)
 # 3 % 2 => 1
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
+#
+# for i in range(10, 20):
+#     print(i)
+#
+# for i in range(10, 20, 2):
+#     print(i)
 
-for i in range(10, 20):
-    print(i)
 
-for i in range(10, 20, 2):
-    print(i)
+for liczba in lista:
+    if liczba < 2: continue
+
+    dzielnik = 2
+    while dzielnik < liczba:
+        if liczba % dzielnik == 0: break
+        dzielnik += 1
+    else:
+        wynik.append(liczba)
+
+print(wynik)
---
+++ cwiczenia/cwiczenie_04.py
@@ -0,0 +1,18 @@
+"""
+N x M
+
+     1   2   3
+
+1    1   2   3
+2    2   4   6
+3    3   6   9
+
+"""
+
+help(print)
+
+print()
+print(1, 2, 3, 4, sep="-", end=":")
+print(1, 2, 3, 4, sep="-")
+x = 10
+print(f"{x:3}")
