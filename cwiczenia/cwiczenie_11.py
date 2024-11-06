c22c5fdb2507d1c9fd9699b542e237ebf43d360b
rkorzen <korzeniewski@gmail.com>
2024-11-06 11:28:07 +0100

    zadanie

--- cwiczenia/cwiczenie_11.py
+++ cwiczenia/cwiczenie_11.py
@@ -4,15 +4,67 @@ określonej liczbie do koszyka. Zaimplementuj metodę obliczającą
 całkowitą wartość koszyka oraz wypisującą informację o zawartości
 koszyka. Dodanie dwa razy tego samego produktu do koszyka
 powinno stworzyć tylko jedną pozycję.
+
 Przykład użycia:
 >>> basket = Basket()
 >>> product = Product(1, 'Woda', 10.00)
+>>> product2 = Product(2, 'Cola', 20.00)
 >>> basket.add_product(product, 5)
+>>> basket.add_product(product2, 10)
 >>> basket.count_total_price()
-50.0
+250.0
 >>> basket.generate_report()
-'Produkty w koszyku:\n
-- Woda (1), cena: 10.00 x 5\n
-W sumie: 50.00
-
+Produkty w koszyku:
+- Woda (1), cena: 10.00 x 5
+- Cola (2), cena: 20.00 x 10
+W sumie: 250.00
 """
+
+
+class Basket:
+    def __init__(self):
+        self.basket_entries = []
+
+    def list_products(self):
+        for basket_entry in self.basket_entries:
+            print(basket_entry)
+
+    def add_product(self, product: "Product", count: int):
+        self.basket_entries.append(BasketEntry(product, count))
+
+    def count_total_price(self):
+        total_price = 0
+        for basket_entry in self.basket_entries:
+            total_price += basket_entry.price()
+        return total_price
+
+    def generate_report(self):
+        raport = "Produkty w koszyku:\n"
+
+        for be in self.basket_entries:
+            raport += be.raport()
+
+        raport += f"W sumie: {self.count_total_price():.2f}"
+        return raport
+
+
+
+class Product:
+    def __init__(self, id: int, name: str, price: float):
+        self.id = id
+        self.name = name
+        self.price = price
+
+
+class BasketEntry:
+    def __init__(self, product: Product, count: int):
+        self.product = product
+        self.count = count
+
+    def price(self) -> float:
+        return self.product.price * self.count
+
+    def raport(self) -> str:
+        return f"- {self.product.name} ({self.product.id}), cena: {self.product.price:.2f} x {self.count}\n"
+
+
---
+++ cwiczenia/test_cwiczenie_11.py
@@ -0,0 +1,23 @@
+from cwiczenie_11 import Basket, Product
+
+
+def test_basket():
+    basket = Basket()
+    product = Product(1, 'Woda', 10.00)
+    product2 = Product(2, 'Cola', 20.00)
+    basket.add_product(product, 5)
+    basket.add_product(product2, 10)
+    assert basket.count_total_price() == 250
+    expected = 'Produkty w koszyku:\n- Woda (1), cena: 10.00 x 5\n- Cola (2), cena: 20.00 x 10\nW sumie: 250.00'
+
+    assert basket.generate_report() == expected
+
+
+def test_basket_add_the_same_product_many_times():
+    basket = Basket()
+    product = Product(1, 'Woda', 10.00)
+    basket.add_product(product, 5)
+    basket.add_product(product, 5)
+    assert basket.count_total_price() == 100
+    expected = 'Produkty w koszyku:\n- Woda (1), cena: 10.00 x 10\nW sumie: 250.00'
+    assert basket.generate_report() == expected
