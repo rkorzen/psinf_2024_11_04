881b35a7a3ec5a45edb9fe584a77c148debc3c45
rkorzen <korzeniewski@gmail.com>
2024-11-06 09:38:21 +0100

    Metody w klasie

--- cwiczenia/cwiczenie_09.py
+++ cwiczenia/cwiczenie_09.py
@@ -8,5 +8,14 @@ Przykład użycia:
 >>> product.print_info()
 Produkt "Woda", id: 1, cena: 10.99 PLN
 
-
 """
+
+class Product:
+
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def print_info(self):
+        print(f'Produkt "{self.name}", id: {self.id}, cena: {self.price} PLN')
