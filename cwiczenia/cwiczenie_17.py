f6f2849d9789bff13f4aa6503fa1c93ff66fe59e
rkorzen <korzeniewski@gmail.com>
2024-11-07 10:40:14 +0100

    cwiczenie_17

--- cwiczenia/cwiczenie_17.py
+++ cwiczenia/cwiczenie_17.py
@@ -1,2 +1,18 @@
+"""
+Napisz program wczytujący plik z logami aktywności użytkowników
+w systemie. Na podstawie wczytanych danych wyświetl informację o
+sumarycznym czasie przebywania każdego użytkownika w systemie.
+Przykład użycia:
+
+$ python read_logs.py
+
+Czas przebywania w systemie:
+- user-1: 92 s
+- user-2: 51 s
+- user-3: 20 s
+
+"""
+
+
 with open("../data/logi.csv") as f:
     print(f.read()[:10])
